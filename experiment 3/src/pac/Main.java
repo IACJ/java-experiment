edf89da730e8ccfc3c815c5ab6a7fec6dc11cb32
ACJ <178480641@qq.com>
2016-11-04 00:41:42 +0800

    exp3 is done(2)

--- experiment 3/src/pac/Main.java
+++ experiment 3/src/pac/Main.java
@@ -11,8 +11,12 @@ public class Main {
 		Scanner input = new Scanner(System.in);
 		int k = input.nextInt();
 		input.close();
-		
+		System.out.println("第一组数据：");
 		K_Means temp1 = new K_Means(k,"./KMeans_Set.txt");
-		temp1.run();	
+		temp1.run();
+		System.out.println();
+		System.out.println("第二组数据：");
+		K_Means temp2 = new K_Means(k,"./KMeans_Set2.txt");
+		temp2.run();	
 	}
 }
